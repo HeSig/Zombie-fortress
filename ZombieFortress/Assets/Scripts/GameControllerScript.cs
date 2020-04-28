a443fba3b0a72d21d2d8c06207c09869fd22c1ce
HeSig <henrik.sigeman@hotmail.se>
2020-04-28 16:15:17 +0200

    new update

---
+++ ZombieFortress/Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerScript : MonoBehaviour
+{
+    GameObject gate; //The player gate
+    GameObject gameCamera; //The game camera
+    public int zombieCount; //Number of zombies currently in the game
+    public GameObject zombieSpawner; //The zombie spawner game object
+    int[] levels = { 5, 10, 15, 20, 25, 30 }; //Number of zombies spawned in each level
+    int currentLevel = 0; //Counter for the current level
+    bool[] levelsCleared = { false, false, false, false, false, false }; //Array of boolean values that show if a level has been completed or not (Can be removed at some point)
+    bool levelRunning;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gate = GameObject.FindGameObjectsWithTag("Finish")[0];
+        gameCamera = GameObject.FindGameObjectsWithTag("MainCamera")[0];
+
+        //zombieSpawner = GameObject.FindGameObjectWithTag("Spawner"); //The zombiespawner object, to be corrected later.
+
+        zombieCount = levels[currentLevel];
+        levelRunning = false;
+        gameCamera.gameObject.transform.Find("LoseText").GetComponent<TextMesh>().text = "Press space to start next level";
+
+
+        //Start first level
+        /*
+        for (int i = 0; i < levelsCleared.Length; i++)
+        {
+            if (levelsCleared[i] == false)
+            {
+                zombieSpawner.GetComponent<ZombieSpawner>().SpawnZombies(levels[i]);
+                currentLevel = i;
+                gameCamera.gameObject.transform.Find("LoseText").GetComponent<TextMesh>().text = "Level " + i;
+                break;
+            }
+        }
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (zombieCount == 0 && levelRunning)
+        {
+            levelsCleared[currentLevel] = true;
+            levelRunning = false;
+            gameCamera.gameObject.transform.Find("LoseText").GetComponent<TextMesh>().text = "Press space to start next level";
+        }
+
+        //If the gate is destroyed the player loses
+        if (gate == null)
+        {
+            gameCamera.gameObject.transform.Find("LoseText").GetComponent<TextMesh>().text = "YOU LOSE";
+        }
+
+        //Checks if there are any zombies left, and if the zombieSpawner has spawned all of its zombies.
+        if (SpacePress())
+        {
+            if (!levelRunning)
+            {
+                Debug.Log("Game start");
+                //Go through the list of levels and choose the next one that's not been completed
+                for (int i = 0; i < levelsCleared.Length; i++)
+                {
+                    if (levelsCleared[i] == false)
+                    {
+                        currentLevel = i;
+                        zombieCount = levels[currentLevel];
+                        levelRunning = true;
+                        //Call the zombie spawner to start spawning zombies according to the level
+                        zombieSpawner.GetComponent<ZombieSpawner>().SpawnZombies(levels[currentLevel]);
+
+                        //Update UI-text to the new level.
+                        gameCamera.gameObject.transform.Find("LoseText").GetComponent<TextMesh>().text = "Level " + (currentLevel + 1);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawCube(transform.position, new Vector3(3, 3, 3));
+    }
+
+    private bool SpacePress()
+    {
+        return Input.GetKeyDown(KeyCode.Space);
+    }
+}
---
+++ ZombieFortress/Assets/Scripts/ZombieScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieScript : MonoBehaviour
+{
+    public float moveSpeed = 0.5f;
+    public float health = 15f;
+    public GameObject nextNode;
+    public int damage = 1;
+    GameObject goal;
+    GameControllerScript mainControllerScript;
+    GameObject mainController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainControllerScript = GameObject.FindGameObjectsWithTag("GameController")[0].GetComponent<GameControllerScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            mainControllerScript.zombieCount -= 1;
+            Destroy(gameObject);
+        }
+
+        if(nextNode != null) {
+            float step = moveSpeed * Time.deltaTime;
+            step = step / 7;
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(nextNode.transform.position.x, transform.position.y, nextNode.transform.position.z), step);
+            if(transform.position.x == nextNode.transform.position.x && transform.position.z == nextNode.transform.position.z)
+            {
+                if(nextNode.tag != "Finish") { 
+                    nextNode = nextNode.GetComponent<WalkNodeScript>().nextNode;
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.gameObject.tag == "Finish")
+        {
+            Debug.Log("Collission");
+            goal = other.gameObject;
+            StartCoroutine("DealGateDamage");
+        }
+        if (other.gameObject.name == "Bullet_Prefab")
+        {
+            Debug.Log("HIT");
+            health -= 1;
+        }
+    }
+
+    IEnumerator DealGateDamage()
+    {
+        goal.GetComponent<GateScript>().health -= damage;
+        yield return new WaitForSeconds(1f);   //Wait
+        StartCoroutine("DealGateDamage");
+    }
+}
