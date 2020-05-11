0875726e16d006b2ef9611d0cba336ce9e1b152a
Johan Steen Wingren <steen81@gmail.com>
2020-05-11 02:04:29 +0200

    added FPS-controls + changes to build UI

---
+++ ZombieFortress/Assets/Scripts/Helper Scripts/TagHolder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axis {
+    public const string HORIZONTAL = "Horizontal";
+    public const string VERTICAL = "Vertical";
+}
+
+public class MouseAxis {
+    public const string MOUSE_X = "Mouse X";
+    public const string MOUSE_Y = "Mouse Y";
+}
+
+public class AnimationTags {
+    public const string SHOOT_TRIGGER = "Shoot";
+}
+
+public class Tags {
+    public const string LOOK_ROOT = "Look Root";
+    public const string ZOOM_CAMERA = "FP Camera";
+    public const string CROSSHAIR = "Crosshair";
+    public const string PLAYER_TAG = "Player";
+    public const string ENEMY_TAG = "Enemy";
+}
