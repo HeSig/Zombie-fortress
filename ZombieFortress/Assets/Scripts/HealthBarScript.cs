c178b0476a3f0e83781447332f287ab4e23d6fd3
Basir98 <47216998+Basir98@users.noreply.github.com>
2020-05-30 23:20:35 +0200

    ljud för empty magazine och UI för reload

---
+++ ZombieFortress/Assets/Scripts/HealthBarScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarScript : MonoBehaviour {
+    //public Gradient gradient;
+    public Slider slider;
+
+    public void SetMaxHealth(float health){
+            slider.maxValue = health;
+            slider.value = health;
+            //healthBar.color = gradient.Evaluate(1f); 
+    }
+
+
+    public void SetHealth(int health){
+        slider.value = health;
+    }
+
+}
